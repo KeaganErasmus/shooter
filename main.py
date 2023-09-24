34be151f667badee2ddb3d22ae7a96faa4ee98c8
keagan erasmus <keaganerasmus12@gmail.com>
2023-09-24 15:47:33 +1300

    Spawing multiple enemies

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import pygame
 
 from player import Player
@@ -6,18 +7,23 @@ from enemy import Enemy
 screen = pygame.display.set_mode((800, 600))
 
 player = Player(screen)
-enemy = Enemy(screen, player, 100, 100)
+enemies = []
+enemy_count = 5
+for i in range(enemy_count):
+    enemies.append(Enemy(screen, player, random.randint(0, 800), random.randint(0, 600) + i))
 
 
 def update(dt):
     player.update_player(dt)
-    enemy.update_enemy(dt)
+    for enemy in enemies:
+        enemy.update_enemy(dt)
 
 
 def render(_screen):
     _screen.fill((255, 255, 255))
     player.draw_player()
-    enemy.draw_enemy(_screen)
+    for enemy in enemies:
+        enemy.draw_enemy(_screen)
 
 
 def main():
